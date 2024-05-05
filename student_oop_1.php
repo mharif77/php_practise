e0f9062deeefdb9ac6528658029d34c0d41901c5
MD ARIF HASAN <mdarifhasan077@gmail.com>
2024-05-06 00:20:57 +0600

    today work

--- student_oop_1.php
+++ student_oop_1.php
@@ -9,43 +9,40 @@
     <?php
     if(isset($_REQUEST['submit'])){
         $st = $_REQUEST['st'];
-       $obj = new Student ('result.txt');
+        $obj = new Student('result.txt');
         echo $obj-> show_result($st);
-
     }
-
     class Student{
         public $datas;
         public $arraydata;
+
         public function __construct($file){
             $this->datas = $file;
             $this->arraydata = file($this->datas);
         }
-        function show_result($st){
+         public function show_result($st){
             foreach($this->arraydata as $data){
-                $line = explode ("|",$data);
-                list($id,$name,$score,$result)= $line;
-                if($id == $st){
+                $line = explode("|", $data);
+                list($id,$name, $score,$result)= $line;
+                if($id == $st ){
                     $output = "id : $id <br>
                     name : $name <br>
                     score : $score <br>
-                    result : $result ";
+                    resutl : $result";
                 }
             }
             return $output;
         }
     }
-
     ?>
-    <form action="" method = "POST" >
-        <select name="st" id="">
-            <option value="">select one</option>
-            <option value="1">Arif</option>
-            <option value="2">joy</option>
-            <option value="3">noman</option>
+    <form action="" method="POST">
+        <select name="" id="">
+            <option value="">choose one</option>
+            <option value="1">humaira</option>
+            <option value="2">nahid</option>
+            <option value="3">mim</option>
         </select>
-        <input type="submit" name = "submit" value ="show value">
+        <input type="submit" name = "submit" value = "show value">
     </form>
-    
 </body>
 </html>
