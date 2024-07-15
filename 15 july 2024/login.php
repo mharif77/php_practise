a18085e41a4235995d6b032a9c01b5b4882619d7
MD ARIF HASAN <mdarifhasan077@gmail.com>
2024-07-16 00:25:01 +0600

    15/07/2024

---
+++ 15 july 2024/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="abc.php" method="$_POST">
+        <input type="text" name="email" placeholder="Enter your email"><br>
+        <input type="text" name="password" placeholder="Enter your password"><br>
+        <input type="submit" name="submit" value="Login">
+
+    </form>
+</body>
+</html>
